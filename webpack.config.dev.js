8bbd00c0ab3226119eb7eac2fef54607cecca0a0
nihau <nicolas.hauseux@gmail.com>
2017-03-27 22:59:51 +0200

    resolve

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,11 @@
-/*global __dirname, require, module*/
+/* global __dirname, require, module */
 
-const webpack = require('webpack');
+require('webpack');
 const path = require('path');
 
 const config = {
   entry: [
-    path.resolve(__dirname,'./sandbox/sandbox.js'),
+    path.resolve(__dirname, './sandbox/sandbox.js'),
   ],
   output: {
     path: __dirname,
@@ -15,7 +15,7 @@ const config = {
   devtool: 'source-map',
   devServer: {
     inline: true,
-    contentBase: path.resolve(__dirname,'./sandbox'),
+    contentBase: path.resolve(__dirname, './sandbox'),
   },
   module: {
     rules: [
@@ -24,7 +24,10 @@ const config = {
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
-    ]
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 };
 
